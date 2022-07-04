20206ae5766dbf5ecb8659c478de6795dce4229a
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2022-07-04 19:35:51 -0300

    movido correçoes

--- Exercicios/Capitulo1/FIX_ME/02.py
+++ Exercicios/Capitulo1/FIX_ME/02.py
@@ -9,7 +9,10 @@ else; print("não tres")
 
 lista_legal = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
 
-if type(lista_legal) == type(list):
+
+if type(lista_legal) == type([]):
     print("é uma lista")
 else:
     print("não é uma lista")
+
+
