e882191ac8ca6cc8ca5f885f0a48de1c91e8c243
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2022-07-30 12:04:23 -0300

    fixed windows issue?

--- Code/GenFile.py
+++ Code/GenFile.py
@@ -15,10 +15,15 @@ class _Getch:
     """Gets a single character from standard input.  Does not echo to the screen."""
 
     def __init__(self) -> None:
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
+        if (system() == "Linux"):
             self.impl = _GetchUnix()
+        elif (system() == "Windows"):
+            self.impl = _GetchWindows()
+        else:
+            try:
+                self.impl = _GetchWindows()
+            except ImportError:
+                self.impl = _GetchUnix()
 
     def __call__(self) -> None:
         return self.impl()
