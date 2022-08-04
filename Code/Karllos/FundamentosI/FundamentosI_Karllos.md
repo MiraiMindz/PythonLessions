1c5f90f97c53dbd391ccaf30cfaa46f7fb5dd954
Karl <79936805+Karllos-Marques@users.noreply.github.com>
2022-08-04 20:52:20 -0300

    class 2

---
+++ Code/Karllos/FundamentosI/FundamentosI_Karllos.md
@@ -0,0 +1,15 @@
+=====================
+dynamically typed
+x = 4
+======================
+static (more specific)
+num x = 3
+======================
+snake_case
+camelCase
+CapitalCase
+dash-case
+======================
+constants are immutable variable
+======================
+
---
+++ Code/Karllos/FundamentosI/FundamentosI_Karllos.py
@@ -0,0 +1,10 @@
+entrada = input
+digite = print
+
+day = entrada('Anata ga umaretahi o oshietekudasai:')
+mouth = entrada('umareta tsuki o oshietekudasai:')
+year = entrada('umareta toshi o oshietekudasai:')
+
+birthday = (day + '/' + mouth + '/' + year)
+
+digite('Anata wa sonohi ni uma reta', birthday)
