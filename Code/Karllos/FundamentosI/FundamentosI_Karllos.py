77b48b2ad22811d408f6a36e5231d631d8c2eb42
Karl <79936805+Karllos-Marques@users.noreply.github.com>
2022-09-08 19:05:33 -0300

    inicio de mais uma aula slk

--- Code/Karllos/FundamentosI/FundamentosI_Karllos.py
+++ Code/Karllos/FundamentosI/FundamentosI_Karllos.py
@@ -169,13 +169,18 @@
 #     print(i)
 #     i += 1
 
-# for num in range(1,101):
+# for num in range(100):
 
-#   if(num % 3 == 0 and num % 5 == 0):
-#     print("'FizzBuzz'")
-#   elif(num % 3 == 0):
-#     print("'Fizz'")
-#   elif(num % 5 == 0):
-#     print("'Buzz'")
-#   else:
-#     print(num)
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("'FizzBuzz'")
+#     elif num % 3 == 0:
+#         print("'Fizz'")
+#     elif num % 5 == 0:
+#         print("'Buzz'")
+#     else:
+#         print(num)
+
+# num = [1, 3, 2]
+# num.sort()
+
+# print(num)
