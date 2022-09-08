9c3286b602ddcf115054f9161c29bb84fe4853d3
Mine1we <79028281+Mine1we@users.noreply.github.com>
2022-09-08 14:24:41 -0300

    Mensagem

--- Code/Mine1we/FundamentosII/FundamentosII_Mine1we.py
+++ Code/Mine1we/FundamentosII/FundamentosII_Mine1we.py
@@ -1,9 +1,49 @@
-os_malucolá = ['Mira!', 'Mirai' , 'Kaal' , 'Zezinho' , 'Ramon']
+#os_malucolá = ['Mira!', 'Mirai' , 'Kaal' , 'Zezinho' , 'Ramon']
 
-nome = input('Insira seu nome: ')
-if nome == 'Mira!':
-    print("'{nome}' é igual a 'Mine'")
-else:
-    print("'{nome}' não é igual a 'Mine'")
+#nome = input('Insira seu nome: ')
+#if nome == 'Mira!':
+#    print("'{nome}' é igual a 'Mine'")
+#else:
+#    print("'{nome}' não é igual a 'Mine'")
 
-    if x != 2:
+#    if x != 2:
+
+from random import randint
+
+
+print ('começando jogo')
+
+
+random = randint(0, 24)
+
+tentativas = 0;
+
+chances = 10;
+
+while tentativas != random:
+    tentativas = input('tenta pensar de maluco um número de 0 a 24:')
+    if tentativas.isnumeric():
+        tentativas = int(tentativas)
+    chances = chances - 1
+    if tentativas == random:
+        print('')
+        print('Parabéns< você conseguiu estranho! O número era {} e você ainda tinha {} chances.'.format(random, chances))  
+        print('')
+        break;
+    else:
+        print('')
+        if tentativas > random:
+            print('Você errou idiota!!! Dica: É um número menor.')
+        else:
+            print('Você errou seu idiota!!! Dica: É um número maior.')
+        print('você ainda tem {} chances.'.format(chances))
+        print('')
+    if tentativas == 0:
+        print ('')
+        print('Suas tentativas acabaram, você perdeu vacilão!')
+        print('')
+        break;
+
+
+
+print( ' o jogo acabou pode kitar bobão')
